f53715e9fe903adbe6fe7bd7146bcafbebc6b9ab
Codeacamp <codeacamp@Daniels-MacBook-Air.local>
2017-02-28 19:41:48 -0600

    Adds shoulda matchers for spec refactors

--- Gemfile
+++ Gemfile
@@ -46,12 +46,14 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  
 end
 
 group :test do
-	gem "rspec-rails", "~> 2.14"
-	gem "factory_girl_rails"
-	gem 'ffaker'
+  gem "rspec-rails", "~> 2.14"
+  gem "factory_girl_rails"
+  gem 'ffaker'
+  gem "shoulda-matchers", '~> 3.1', require: false
 end
 
 gem "devise"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,20 @@ describe User do
  before { @user = FactoryGirl.build(:user) }
 
  subject { @user }
+ 
+ # it { should respond_to(:email) }
+ #  it { should respond_to(:password) }
+ #  it { should respond_to(:password_confirmation) }
 
- it { should respond_to(:email) }
- it { should respond_to(:password) }
- it { should respond_to(:password_confirmation) }
+ #  it { should be_valid }
+   it { should validate_presence_of(:email) }
+ it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
 
- it { should be_valid }
+ it { should validate_confirmation_of(:password) }
+ it { should allow_value('example@domain.com').for(:email) }
+
 end
+
+# describe 'when email is not present' do
+
+# end
